f4a579bee2a2194df9fb3cbc3f801a7c3b66c960
Sergii Sheikin <sergii.sheikin@gmail.com>
2023-11-16 14:12:25 +0200

    Перероблено конфігурації для  ast+freepbx

--- iptables.sh
+++ iptables.sh
@@ -6,19 +6,28 @@ systemctl start iptables
 systemctl enable iptables
 
 iptables -F &&
-iptables -A INPUT -i lo -j ACCEPT &&
-iptables -A INPUT -p tcp --dport 22 -j ACCEPT &&
-iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT &&
+iptables -A INPUT -i lo -j ACCEPT && 
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT && 
+iptables -A INPUT -s 10.33.85.0/24 -p tcp -m state --state NEW -m tcp --dport 22 -j ACCEPT &&
+iptables -A INPUT -s 10.33.85.0/24 -p icmp -m icmp --icmp-type 8 -j ACCEPT &&
+iptables -A INPUT -s 10.33.85.0/24 -p tcp -m tcp --dport 80 -j ACCEPT &&
+iptables -A INPUT -s 10.33.85.0/24 -p tcp -m tcp --dport 8033 -j ACCEPT &&
+iptables -A INPUT -s 10.33.85.0/24 -p tcp -m tcp --dport 8080 -j ACCEPT &&
+iptables -A INPUT -s 10.33.85.0/24 -p tcp -m state --state NEW -m tcp --dport 3306 -j ACCEPT &&
+iptables -A INPUT -s 10.33.84.5/24 -p tcp -m state --state NEW -m tcp --dport 22 -j ACCEPT &&
+iptables -A INPUT -s 10.33.84.5/24 -p icmp -m icmp --icmp-type 8 -j ACCEPT &&
+iptables -A INPUT -s 10.33.84.5/24 -p tcp -m tcp --dport 80 -j ACCEPT &&
+iptables -A INPUT -s 10.33.84.5/24 -p tcp -m tcp --dport 8033 -j ACCEPT &&
+iptables -A INPUT -s 10.33.84.5/24 -p tcp -m tcp --dport 8080 -j ACCEPT &&
+iptables -A INPUT -s 10.33.84.5/24 -p tcp -m state --state NEW -m tcp --dport 3306 -j ACCEPT &&
+iptables -A INPUT -s 10.0.0.0/24 -p udp -m udp --dport 5060 -j ACCEPT &&
+iptables -A INPUT -s 10.0.0.0/24 -p udp -m udp --dport 10000:50000 -j ACCEPT &&
+iptables -A INPUT -s 10.0.0.0/24 -p udp -m udp --dport 69 -j ACCEPT &&
 iptables -P INPUT DROP &&
-iptables -P FORWARD DROP &&
-iptables -P OUTPUT ACCEPT &&
-iptables -A INPUT -p icmp --icmp-type echo-request -j ACCEPT &&
-iptables -A OUTPUT -p icmp --icmp-type echo-reply -j ACCEPT &&
-iptables -A INPUT -p udp -m udp --dport 5060 -j ACCEPT &&
-iptables -A INPUT -p udp -m udp --dport 69 -j ACCEPT &&
-iptables -A INPUT -p udp -m udp --dport 10000:20000 -j ACCEPT &&
-service iptables save
+iptables -P FORWARD DROP && 
+iptables -P OUTPUT ACCEPT && 
+service iptables save && 
+iptables -nvL
 
-echo "Налаштування iptables завершено!"
 
 exit 0
--- start.sh
+++ start.sh
@@ -20,53 +20,95 @@ systemctl disable firewalld
 systemctl stop NetworkManager
 systemctl disable NetworkManager	
 
-# Обновити
 yum -y update
-yum -y install epel-release
-yum -y install wget mc vim gcc gcc-c++ libedit-devel sqlite-devel jansson-devel libxml2-devel libuuid-devel bzip2 patch kernel-devel-`uname -r` kernel-headers-`uname -r` newt-devel	
+yum -y groupinstall core base "Development Tools"
 
-# Install Asterisk
-cd /usr/src/
-wget http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-18-current.tar.gz
-tar xvfz asterisk-18-current.tar.gz
-rm -f asterisk-18-current.tar.gz
-cd /usr/src/asterisk-18*/
-contrib/scripts/install_prereq install
-./configure --with-jansson-bundled --libdir=/usr/lib64 
-make menuselect	
+yum -y install lynx mariadb-server mariadb php php-mysql php-mbstring tftp-server \
+  httpd ncurses-devel sendmail sendmail-cf sox newt-devel libxml2-devel libtiff-devel \
+  audiofile-devel gtk2-devel subversion kernel-devel git php-process crontabs cronie \
+  cronie-anacron wget vim php-xml uuid-devel sqlite-devel net-tools gnutls-devel php-pear unixODBC mysql-connector-odbc
+
+pear install Console_Getopt
+
+systemctl enable mariadb.service
+systemctl start mariadb
+mysql_secure_installation
+
+systemctl enable httpd.service
+systemctl start httpd.service
+
+adduser asterisk -m -c "Asterisk User"
+
+cd /usr/src
+wget http://downloads.asterisk.org/pub/telephony/dahdi-linux-complete/dahdi-linux-complete-current.tar.gz
+wget http://downloads.asterisk.org/pub/telephony/libpri/libpri-1-current.tar.gz
+wget https://downloads.asterisk.org/pub/telephony/asterisk/old-releases/asterisk-13.38.3.tar.gz
+wget -O jansson.tar.gz https://github.com/akheron/jansson/archive/v2.7.tar.gz
+wget https://github.com/pjsip/pjproject/archive/refs/tags/2.4.tar.gz
+
+cd /usr/src
+tar xvfz dahdi-linux-complete-current.tar.gz
+tar xvfz libpri-1-current.tar.gz
+rm -f dahdi-linux-complete-current.tar.gz libpri-1-current.tar.gz
+cd dahdi-linux-complete-*
+make all
+make install
+make config
+cd /usr/src/libpri-*
 make
 make install
-# якщо make samples не робити то небуде конфігураційних файлів
-make samples
+
+cd /usr/src
+tar -xjvf pjproject-2.4.tar.bz2
+rm -f pjproject-2.4.tar.bz2
+cd pjproject-2.4
+CFLAGS='-DPJ_HAS_IPV6=1' ./configure --prefix=/usr --enable-shared --disable-sound\
+  --disable-resample --disable-video --disable-opencore-amr --libdir=/usr/lib64
+make dep
+make
+make install
+
+cd /usr/src
+tar vxfz jansson.tar.gz
+rm -f jansson.tar.gz
+cd jansson-*
+autoreconf -i
+./configure --libdir=/usr/lib64
+make
+make install
+
+cd /usr/src
+tar xvfz asterisk-13.38.3.tar.gz
+rm -f asterisk-13.38.3.tar.gz
+cd asterisk-*
+contrib/scripts/install_prereq install
+./configure --libdir=/usr/lib64
+contrib/scripts/get_mp3_source.sh
+make menuselect
+make
+make install
 make config
 ldconfig
+chkconfig asterisk off
 
-groupadd asterisk 
-useradd -r -d /var/lib/asterisk -g asterisk asterisk 
-usermod -aG audio,dialout asterisk 
-chown -R asterisk.asterisk /etc/asterisk 
-chown -R asterisk.asterisk /var/{lib,log,spool}/asterisk
-chown -R asterisk.asterisk /usr/lib64/asterisk
-chmod -R 755 /var/{lib,log,run,spool}/asterisk /usr/lib64/asterisk /etc/asterisk
-	
-# Розкоментувати AST_USER і AST_GROUP у /etc/sysconfig/asterisk
-sed -i 's/#AST_USER="asterisk"/AST_USER="asterisk"/' /etc/sysconfig/asterisk
-sed -i 's/#AST_GROUP="asterisk"/AST_GROUP="asterisk"/' /etc/sysconfig/asterisk
+chown asterisk. /var/run/asterisk
+chown -R asterisk. /etc/asterisk
+chown -R asterisk. /var/{lib,log,spool}/asterisk
+chown -R asterisk. /usr/lib64/asterisk
+chown -R asterisk. /var/www/
 
-# Розкоментувати та встановити значення runuser та rungroup у /etc/asterisk/asterisk.conf
-sed -i 's/;runuser = asterisk/runuser = asterisk/' /etc/asterisk/asterisk.conf
-sed -i 's/;rungroup = asterisk/rungroup = asterisk/' /etc/asterisk/asterisk.conf
+sed -i 's/\(^upload_max_filesize = \).*/\120M/' /etc/php.ini
+sed -i 's/^\(User\|Group\).*/\1 asterisk/' /etc/httpd/conf/httpd.conf
+sed -i 's/AllowOverride None/AllowOverride All/' /etc/httpd/conf/httpd.conf
+systemctl restart httpd.service
 
-systemctl enable asterisk
-systemctl start asterisk
-
-# Створюємо користувача із заданим ім'ям та паролем
-useradd -m -s /bin/bash "$USERNAME"
-echo "$USERNAME:$PASSWORD" | chpasswd
-
-# Додаємо налаштування SSH до конфігураційного файлу
-echo "PermitRootLogin no" >> /etc/ssh/sshd_config
-echo "AllowUsers $USERNAME" >> /etc/ssh/sshd_config
+cd /usr/src
+wget http://mirror.freepbx.org/modules/packages/freepbx/freepbx-13.0-latest.tgz
+tar xfz freepbx-13.0-latest.tgz
+rm -f freepbx-13.0-latest.tgz
+cd freepbx
+./start_asterisk start
+./install -n
 
 echo "Налаштування завершено."
 
