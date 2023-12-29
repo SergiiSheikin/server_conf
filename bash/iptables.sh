628b0cfdeec66aa59c1cecca56ee874307074974
Sergii Sheikin <sergii.sheikin@gmail.com>
2023-12-29 16:01:24 +0200

    hide something

--- bash/iptables.sh
+++ bash/iptables.sh
@@ -8,21 +8,15 @@ systemctl enable iptables
 iptables -F &&
 iptables -A INPUT -i lo -j ACCEPT && 
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT && 
-iptables -A INPUT -s 10.33.85.0/25 -p tcp -m state --state NEW -m tcp --dport 22 -j ACCEPT &&
-iptables -A INPUT -s 10.33.85.0/25 -p icmp -m icmp --icmp-type 8 -j ACCEPT &&
-iptables -A INPUT -s 10.33.85.0/25 -p tcp -m tcp --dport 80 -j ACCEPT &&
-iptables -A INPUT -s 10.33.85.0/25 -p tcp -m tcp --dport 8033 -j ACCEPT &&
-iptables -A INPUT -s 10.33.85.0/25 -p tcp -m tcp --dport 8080 -j ACCEPT &&
-iptables -A INPUT -s 10.33.85.0/25 -p tcp -m state --state NEW -m tcp --dport 3306 -j ACCEPT &&
-iptables -A INPUT -s 10.33.84.5/25 -p tcp -m state --state NEW -m tcp --dport 22 -j ACCEPT &&
-iptables -A INPUT -s 10.33.84.5/25 -p icmp -m icmp --icmp-type 8 -j ACCEPT &&
-iptables -A INPUT -s 10.33.84.5/25 -p tcp -m tcp --dport 80 -j ACCEPT &&
-iptables -A INPUT -s 10.33.84.5/25 -p tcp -m tcp --dport 8033 -j ACCEPT &&
-iptables -A INPUT -s 10.33.84.5/25 -p tcp -m tcp --dport 8080 -j ACCEPT &&
-iptables -A INPUT -s 10.33.84.5/25 -p tcp -m state --state NEW -m tcp --dport 3306 -j ACCEPT &&
-iptables -A INPUT -s 10.0.0.0/8 -p udp -m udp --dport 5060 -j ACCEPT &&
-iptables -A INPUT -s 10.0.0.0/8 -p udp -m udp --dport 10000:50000 -j ACCEPT &&
-iptables -A INPUT -s 10.0.0.0/8 -p udp -m udp --dport 69 -j ACCEPT &&
+iptables -A INPUT -p tcp -m state --state NEW -m tcp --dport 22 -j ACCEPT &&
+iptables -A INPUT -p icmp -m icmp --icmp-type 8 -j ACCEPT &&
+iptables -A INPUT -p tcp -m tcp --dport 80 -j ACCEPT &&
+iptables -A INPUT -p tcp -m tcp --dport 8033 -j ACCEPT &&
+iptables -A INPUT -p tcp -m tcp --dport 8080 -j ACCEPT &&
+iptables -A INPUT -p tcp -m state --state NEW -m tcp --dport 3306 -j ACCEPT &&
+iptables -A INPUT -p udp -m udp --dport 5060 -j ACCEPT &&
+iptables -A INPUT -p udp -m udp --dport 10000:50000 -j ACCEPT &&
+iptables -A INPUT -p udp -m udp --dport 69 -j ACCEPT &&
 iptables -P INPUT DROP &&
 iptables -P FORWARD DROP && 
 iptables -P OUTPUT ACCEPT && 
--- bash/setup_vlan30.sh
+++ bash/setup_vlan30.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+source .env
 sudo yum -y remove NetworkManager NetworkManager-libnm NetworkManager-team NetworkManager-tui NetworkManager-wifi
 # Встановлення модуля 8021q
 sudo modprobe 8021q
@@ -12,17 +12,17 @@ sudo bash -c 'cat <<EOL > /etc/sysconfig/network-scripts/ifcfg-enp0s3.30
 DEVICE=enp0s3.30
 BOOTPROTO=none
 ONBOOT=yes
-IPADDR=10.33.84.5
-NETMASK=255.255.255.128
-NETWORK=10.33.84.0
+IPADDR='$IPADDRVLAN'
+NETMASK='$NETMASKVLAN'
+NETWORK='$NETWORKVLAN'
 VLAN=yes
 EOL'
 
 # Створення файлу /etc/sysconfig/network-scripts/rule-enp0s3.30
-sudo bash -c 'echo "from 10.33.84.0/25 tab 1 priority 500" > /etc/sysconfig/network-scripts/rule-enp0s3.30'
+sudo bash -c 'echo "from '$RULEVLAN' tab 1 priority 500" > /etc/sysconfig/network-scripts/rule-enp0s3.30'
 
 # Створення файлу /etc/sysconfig/network-scripts/route-enp0s3.30
-sudo bash -c 'echo "default via 10.33.84.1 dev enp0s3.30 table 1" > /etc/sysconfig/network-scripts/route-enp0s3.30'
+sudo bash -c 'echo "default via '$ROUTVLAN' dev enp0s3.30 table 1" > /etc/sysconfig/network-scripts/route-enp0s3.30'
 
 # Створення файлу /etc/sysctl.d/90-override.conf
 sudo bash -c 'cat <<EOL > /etc/sysctl.d/90-override.conf
